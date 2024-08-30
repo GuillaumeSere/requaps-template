484b8938fe2d923d4a65a17c5ba92aeac42d64c1
Guillaume Sere <guillaumesere@hotmail.fr>
2024-08-30 11:19:47 +0200

    add footer

--- src/screens/PageNotFound/PageNotFound.jsx
+++ src/screens/PageNotFound/PageNotFound.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const PageNotFound = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default PageNotFound
+    return (
+      <div className="bg-errie-black min-h-screen p-4 flex items-center justify-center">
+        <h3 className="text-white lg:text-4xl md:text-3xl sm:text-2xl text-xl font-semibold">Page Not Found !</h3>
+      </div>
+    )
+  }
+  
+  export default PageNotFound
