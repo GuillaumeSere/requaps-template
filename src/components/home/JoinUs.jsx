52c09e67aebca4a11f633d85593b80fd6b9dc631
Guillaume Sere <guillaumesere@hotmail.fr>
2024-08-30 11:11:25 +0200

    first commit

---
+++ src/components/home/JoinUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JoinUs = () => {
+  return (
+    <section className="py-[65px] lg:min-h-[680px] min-h-auto px-4 text-white relative after:content-[''] after:bg-grad-theme-135">
+      
+    </section>
+  )
+}
+
+export default JoinUs
